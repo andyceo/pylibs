45cab63547e156e35ecacd0411acc0b6568c8c06
andyceo <andyceo@yandex.ru>
2017-08-28 19:28:21 +0300

    Improve config module - it can now load '_file'-terminated keys to config

--- config.py
+++ config.py
@@ -7,3 +7,10 @@ import sys
 config = configparser.ConfigParser()
 config_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
 config.read([config_dir + '/config-sample.ini', config_dir + '/config.ini'])
+
+for section in config.sections():
+    for (k, v) in config.items(section):
+        key = k[:-5]
+        if k[-5:] == '_file' and not config.has_option(section, key):
+            with open(config[section][k], 'r') as file:
+                config[section][key] = file.read().strip(' \t\n\r')
