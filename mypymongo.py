faeba5ac512b1f3b8878bc5c90418349cf0cfed6
andyceo <andyceo@yandex.ru>
2017-09-03 17:16:57 +0300

    Update module mypumongo with handful functions for update timestamp fields and remove changelog fields

--- mypymongo.py
+++ mypymongo.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient
 from pylibs import config
+from pylibs import utils
 
 
 def connect(section, collection=None):
@@ -15,3 +16,38 @@ def connect(section, collection=None):
         return client[db]
     else:
         return client[db][collection]
+
+
+def excluded_fields(db, collection):
+    excluded_fields = {
+        'mongo-alfadirect': {
+            'bitfinex_offers': {
+                '_id': False,
+                'changelog': False,
+            }
+        }
+    }
+    return excluded_fields[db][collection]
+
+
+def update_timestamps(collection, id_field_name='_id', timestamp_field_name='timestamp'):
+    for document in collection.find({}):
+        ts = utils.timestamp_normalize(document[timestamp_field_name])
+        if ts != document[timestamp_field_name]:
+            collection.update_one({id_field_name: document[id_field_name]}, {'$set': {
+                'timestamp': ts,
+            }})
+            utils.message('Updated: id {}, old {}, new {}'.format(document[id_field_name], document['timestamp'], ts))
+        else:
+            utils.message('Unchanged document id {}, {}'.format(document[id_field_name], document['timestamp']))
+
+
+def remove_changelogs(collection, id_field_name='_id', changelog_field_name='changelog'):
+    for document in collection.find({}):
+        if changelog_field_name in document:
+            collection.update_one({id_field_name: document[id_field_name]}, {'$unset': {
+                changelog_field_name: 1,
+            }})
+            utils.message('Removed field {} in document id {}'.format(changelog_field_name, document[id_field_name]))
+        else:
+            utils.message('Unchanged document id {}'.format(document[id_field_name]))
