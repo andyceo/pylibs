1348c876e9fdf32a36f4938ba88a2356e56a7d61
andyceo <andyceo@yandex.ru>
2017-12-31 19:39:41 +0300

    Add function for moving points from one database (and/or measurement) to another one

--- myinfluxdb.py
+++ myinfluxdb.py
@@ -1,3 +1,4 @@
+import copy
 from influxdb import InfluxDBClient
 from pylibs import config
 
@@ -20,3 +21,71 @@ def write(client, points, time_precision=None):
         end = i + batch_size - 1
         end = l if end > l else end
         client.write_points(points[i:end], time_precision=time_precision)
+
+
+def move_points(source, dest):
+    """
+    This function helps transfer points from one database (and/or measurement) to another one. Here is the demo script
+    using that function:
+
+
+    import pylibs
+
+    source = {
+        'client': pylibs.connect({
+            'host': 'influxdb_source',
+            'username': 'user1',
+            'password': 'super_secret_password',
+            'database': 'some_database'
+        }),
+        'measurement': 'dockerhub',
+        'fields': ['field_1', 'field_2', 'another_field'],
+        'tags': ['tag_1', 'tag_2']
+    }
+
+    dest = pylibs.connect({
+        'host': 'influxdb_dest',
+        'username': 'user2',
+        'password': 'another_super_secret_password',
+        'database': 'another_database'
+    })
+
+    pylibs.move_points(source, dest)
+
+
+    :param source: Dictionary with source measurement description.
+    :param dest: Destination client or dictionary with destination measurement description.
+    :return:
+    """
+
+    if not isinstance(dest, dict):
+        dest = {'client': dest}
+
+    if 'client' not in source or 'client' not in dest:
+        print("Source and destinations clients must be passed in!")
+        exit(1)
+
+    if 'measurement' not in source:
+        print("Source measurement must be passed in!")
+        exit(2)
+    elif 'measurement' not in dest:
+        dest['measurement'] = source['measurement']
+
+    res = source['client'].query("SELECT * FROM {}".format(source['measurement']))
+
+    points = []
+    point_template = {
+        "time": None,
+        "measurement": dest['measurement'],
+        "tags": {},
+        "fields": {},
+    }
+    for point in res.get_points():
+        point_raw = copy.deepcopy(point_template)
+        point_raw['time'] = point['time']
+        for meta_key in ['fields', 'tags']:
+            for key in source[meta_key]:
+                point_raw[meta_key][key] = point[key]
+        points.append(point_raw)
+
+    write(dest['client'], points)
