92e0722adacae12f43a02c4fd71b5c37e3d38ea0
andyceo <andyceo@yandex.ru>
2019-09-29 14:58:50 +0300

    Rewrite rub2btc script

--- cryptobalance/rub2btc.py
+++ cryptobalance/rub2btc.py
@@ -4,29 +4,42 @@
 with current exchange rates."""
 
 import argparse
+import os
 import requests
 
-BINANCE_BTCUSDT_URL = 'https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT'
-CBRF = 'https://www.cbr-xml-daily.ru/daily_json.js'
-COMISSION = 0.03  # 3%
+
+def btcusdt():
+    req = requests.get('https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT', timeout=5)
+    return float(req.json()['price'])
+
+
+def usdrub():
+    req = requests.get('https://www.cbr-xml-daily.ru/daily_json.js', timeout=5)
+    return float(req.json()['Valute']['USD']['Value'])
+
+
+def btcrub(btcusd, usdrub):
+    return round(btcusd * usdrub, 2)
+
+
+def btcamount(rubamount, btcusd, usdrub, fee):
+    return rubamount / btcusd / usdrub / (1-fee/100)
+
 
 if __name__ == '__main__':
-    req = requests.get(BINANCE_BTCUSDT_URL, timeout=5)
-    btcusdt = float(req.json()['price'])
-
-    req = requests.get(CBRF, timeout=5)
-    usdrub = float(req.json()['Valute']['USD']['Value'])
+    btcusdt = btcusdt()
+    usdrub = usdrub()
 
     print('BTC/USD:', btcusdt)
     print('USD/RUB:', usdrub)
-    print('BTC/RUB:', btcusdt * usdrub)
-
-    c = 1 - COMISSION
+    print('BTC/RUB:', btcrub(btcusdt, usdrub))
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('amounts', metavar='N', type=int, nargs='+', help='Amounts to calculate')
+    parser.add_argument('--fee', metavar='FEE', default=os.environ.get('FEE', 3), help='Fee, in percent')
     args = parser.parse_args()
+    c = 1-args.fee/100
 
     for amount in args.amounts:
-        res = amount / btcusdt / usdrub / c
+        res = btcamount(amount, btcusdt, usdrub, args.fee)
         print('{} / {} / {} / {} = {}'.format(amount, btcusdt, usdrub, c, res))
