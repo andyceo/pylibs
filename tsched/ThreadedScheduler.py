22ec3da9cd3446770c23a59fc2134af5f24f4b4c
andyceo <andyceo@yandex.ru>
2019-12-01 16:42:56 +0300

    Little style fixes

--- tsched/ThreadedScheduler.py
+++ tsched/ThreadedScheduler.py
@@ -59,7 +59,7 @@ class ThreadedScheduler(threading.Thread):
             logging.info('Scheduler queue cleaned!')
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     logging.basicConfig(
         level=logging.INFO,
         format="[%(asctime)s] %(levelname)s [%(name)s.%(module)s.%(funcName)s:%(lineno)d] %(message)s",
