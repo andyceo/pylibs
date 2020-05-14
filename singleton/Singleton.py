a00404a52b5190fdcb3787a6da337d2b4268f6f0
andyceo <andyceo@yandex.ru>
2020-05-14 15:27:13 +0300

    Added new module singleton with Singleton pattern realisation via metaclass

---
+++ singleton/Singleton.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This file contains a metaclass Singleton that should be used in singleton classes.
+Examples:
+
+    class Logger(metaclass=Singleton):
+        pass
+
+If you want to run __init__ every time the class is called, add
+
+    else:
+        cls._instances[cls].__init__(*args, **kwargs)
+
+to the if statement in Singleton.__call__.
+
+@see https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python"""
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
