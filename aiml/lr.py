243d1d0a266adbae5c40beac469193e80d9691f8
andyceo <andyceo@yandex.ru>
2020-08-07 19:07:44 +0300

    Add optimized version of Gradient Descent realisation

--- aiml/lr.py
+++ aiml/lr.py
@@ -52,3 +52,19 @@ def gradient_descent(x: np.ndarray, y: np.ndarray, theta: np.ndarray, alpha: flo
             theta[j, 0] = theta[j, 0] - alpha * sum(np.multiply(x @ theta - y, x[:, j].reshape(m, 1))) / m
         j_history[i] = compute_cost(x, y, theta)
     return theta, j_history
+
+
+def gd(x: np.ndarray, y: np.ndarray, theta: np.ndarray, alpha: float, num_iters: int) \
+        -> (np.ndarray, np.ndarray):
+    """Optimized version of gradient_descent function. Parameters and return is equal with gradient_descent()"""
+    m = len(y)  # number of training examples
+    n = x.shape[1]  # number of features
+    alpha_m = alpha / m  # constant formula part, calculate outside of loops
+    j_history = np.zeros(shape=(num_iters, 1))
+    for i in range(num_iters):
+        difference = x @ theta - y
+        for j in range(n):
+            # np.multiply is the element-wise multiplication
+            theta[j, 0] = theta[j, 0] - alpha_m * sum(np.multiply(difference, x[:, j].reshape(m, 1)))
+        j_history[i] = np.ndarray.item(sum(np.square(difference)) / (2 * m))  # use precalculated difference
+    return theta, j_history
