1c24415cc72d67b40f1881f3797c165d21a3c8c7
andyceo <andyceo@yandex.ru>
2020-08-07 18:55:26 +0300

    Add gradient_descent() - straitforward realisation

--- aiml/lr.py
+++ aiml/lr.py
@@ -23,7 +23,32 @@ def compute_cost(x: np.ndarray, y: np.ndarray, theta: np.ndarray) -> float:
     :param x: set of features
     :param y: set of training examples
     :param theta: parameters of linear regression function
-
     """
     m = len(y)  # number of training examples
     return np.ndarray.item(sum(np.square(x @ theta - y)) / (2 * m))
+
+
+def gradient_descent(x: np.ndarray, y: np.ndarray, theta: np.ndarray, alpha: float, num_iters: int) \
+        -> (np.ndarray, np.ndarray):
+    """Straitforward (unoptimized) version of Gradient Descent. Applies num_iters iterations with learning rate alpha
+    and returns tuple (theta, J_history) with finded theta and history of values of the cost function J_history
+
+    If x.shape = (m, n), then y.shape must be (m, 1) and theta.shape must be (1, n)
+
+    :param x: set of features
+    :param y: set of training examples
+    :param theta: parameters of linear regression function
+    :param alpha: learning rate alpha (size of the one step of gradient descent)
+    :param num_iters: number of iterations for gradient descent
+    :return: a tuple (theta, J_historu), where theta is sought-for parameter of Linear Regression Model,
+        J_history - is an array of cost function values over iterations
+    """
+    m = len(y)  # number of training examples
+    n = x.shape[1]  # number of features
+    j_history = np.zeros(shape=(num_iters, 1))
+    for i in range(num_iters):
+        for j in range(n):
+            # np.multiply is the element-wise multiplication
+            theta[j, 0] = theta[j, 0] - alpha * sum(np.multiply(x @ theta - y, x[:, j].reshape(m, 1))) / m
+        j_history[i] = compute_cost(x, y, theta)
+    return theta, j_history
