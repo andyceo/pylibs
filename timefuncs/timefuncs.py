e2e9b8b6662605f9590f14ea0b18a6bfda155b4e
andyceo <andyceo@yandex.ru>
2020-08-08 14:00:20 +0300

    timefuncs: refactor gmtstr->gmtdt. Also add if no timestamp given, use current timestamp

--- timefuncs/timefuncs.py
+++ timefuncs/timefuncs.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """Bunch of date and time functions"""
 import datetime
+import time
 
 
-def gmtstr(ts: float) -> str:
-    """Convert given timestamp into GMT ISO date string. Timestamp treated as UTC"""
+def gmtdt(ts=time.time()) -> str:
+    """Convert given timestamp into GMT ISO date string. Timestamp treated as UTC. If no timestamp given, use current"""
     return datetime.datetime.fromtimestamp(ts, tz=datetime.timezone.utc).replace(microsecond=0).isoformat()
